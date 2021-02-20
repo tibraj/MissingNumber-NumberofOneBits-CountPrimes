4de898bccc60834aea24db0d7430fbe613a49033
tibraj <tibraj94@gmail.com>
2021-02-20 13:41:00 -0500

    count primes

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ function missingNumber(nums) {
 //Number of One Bits
 
 //Count Primes
+const countPrimes = (n, count = 0, array =[]) => {
+    for(let i = 2; i < n; i++) {
+        if(array[i]) continue 
+        else count++
+        for(let j = i + i; j <= n; j += i) array[j] = true 
+    }
+    return count;
+}
